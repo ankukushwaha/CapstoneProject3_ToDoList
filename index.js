62c6cd2bdee02af9ee4d1a399744b03d5f443b2e
ankukushwaha <ankurkushwaha7408@gmail.com>
2023-10-18 20:26:47 +0530

    Third commit

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser"; 
+import mongoose from "mongoose";
+import _ from "lodash";
 
 const app = express();
 var port = 3000;
@@ -7,44 +9,114 @@ var port = 3000;
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
-var tasks1 = [];
-var tasks2 = [];
+mongoose.connect("mongodb://127.0.0.1:27017/toDoListDB");
+
+const toDoSchema = new mongoose.Schema({
+    name: String
+})
+
+const ToDoWork = mongoose.model('ToDoWork', toDoSchema);
+const ToDoToday = mongoose.model('ToDoToday', toDoSchema);
+
+const listSchema = new mongoose.Schema({
+    name : String, 
+    items: [toDoSchema]
+})
+const List = mongoose.model('List', listSchema);
+
 let alertMessage = null;
+
+var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+var month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+var d = new Date();
+var dayName = days[d.getDay()];
+var monthName = month[d.getMonth()];
+var date = d.getDate() + " " + monthName + " " + d.getFullYear() + " " + `(${dayName})`;
 
 app.get("/", (req, res) => {
     res.render("index.ejs");
 })
 
 app.get("/click", (req, res) => {
-    var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    var month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-    var d = new Date();
-    var dayName = days[d.getDay()];
-    var monthName = month[d.getMonth()];
-    var date = d.getDate() + " " + monthName + " " + d.getFullYear() + " " + `(${dayName})`;
-    res.render("partials/work.ejs", {DateName: date, addTask: tasks1, alertMsg: alertMessage});
+    ToDoWork.find({}).then((tasks1) => {
+        res.render("partials/work.ejs", {DateName: date, addTask: tasks1, alertMsg: alertMessage, heading:"Work"});
+    });
 })
 
 app.get("/today", (req, res) => {
-    var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    var month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-    var d = new Date();
-    var dayName = days[d.getDay()];
-    var monthName = month[d.getMonth()];
-    var date = d.getDate() + " " + monthName + " " + d.getFullYear() + " " + `(${dayName})`;
-    res.render("partials/today.ejs", {DateName: date, addTask: tasks2, alertMsg: alertMessage});
+    ToDoToday.find({}).then((tasks2) => {
+        // console.log(tasks2); 
+        res.render("partials/today.ejs", {DateName: date, addTask: tasks2, alertMsg: alertMessage, heading:"Today"});
+    });
 })
+
+
+app.get("/:customListName", (req,res) => {
+    const customListName = _.capitalize(req.params.customListName);
+    List.findOne({name: customListName}).then((foundList) => {
+        if(foundList){
+            // console.log(foundList);   
+            res.render("partials/work.ejs", {DateName: date, addTask: foundList.items, alertMsg: alertMessage, heading: foundList.name});
+        }
+        else{
+            const list = new List({
+                name: customListName
+            })
+            list.save();
+            res.redirect("/" + customListName);
+            // console.log("not exists");
+        }
+    }).catch((err) => {console.log(err)});
+})
+
 
 app.post("/add1", (req, res) => {
     var task = req.body.text;
+    const listName = req.body.list;
+    // console.log(listName);
+
+    const tasks = new ToDoWork({
+        name: task
+    });
+
     if(task === ""){
         alertMessage = "Please fill it first!";
+        res.redirect("/click");
     }
     else{
-        tasks1.push(task);
         alertMessage = null;
+
+        if(listName === "Work"){
+            tasks.save();
+            res.redirect("/click");
+        }
+        else{
+            List.findOne({name: listName}).then((foundList) => {
+                foundList.items.push(tasks);
+                foundList.save();
+                res.redirect("/"+listName);
+            })
+            .catch((err) => console.log(err));
+        }
     }
-    res.redirect("/click");
+})
+
+app.post("/deleteWork", (req,res) => {
+    const id = req.body.checkbox;
+    const listName = req.body.listName;
+    // console.log(listName); 
+
+    if(listName === "Work"){
+        ToDoWork.findByIdAndRemove(id).then(() => console.log("Successfully deleted"))
+        .catch((err) => console.log(err));
+        res.redirect("/click");
+    }
+    else{
+        List.findOneAndUpdate({name:listName}, {$pull : {items: {_id: id}}}).then(() => {
+            console.log("deleted sucessfully");
+            res.redirect("/"+listName);
+        })
+    }
 })
 
 app.post("/add2", (req, res) => {
@@ -53,12 +125,24 @@ app.post("/add2", (req, res) => {
         alertMessage = "Please fill it first!";
     }
     else{
-        tasks2.push(task);
+        const tasks = new ToDoToday({
+            name: task
+        })
+        tasks.save();
         alertMessage = null;
     }
-    res.redirect("/today");
-    
+    res.redirect("/today");   
 })
+
+
+app.post("/deleteToday", (req,res) => {
+    ToDoToday.findByIdAndRemove(req.body.checkbox).then(() => console.log("successfully deleted"))
+    .catch((err) => console.log(err));
+    res.redirect("/today");
+})
+
+
+
 
 app.listen(port, () => {
     console.log(`Server is successfully running at port ${port}.`);
