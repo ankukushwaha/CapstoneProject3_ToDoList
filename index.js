2883fa143377d7b0d06d080b9a7ab3eaa9fed86d
ankukushwaha <ankurkushwaha7408@gmail.com>
2023-10-19 13:34:45 +0530

    Fifth commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 import _ from "lodash";
 
 const app = express();
-var port = process.env.PORT || 3000;
+var port = process.env.port || 3000;
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
